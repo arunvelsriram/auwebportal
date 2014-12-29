640ae0761abf443925d7b2a549925b55d89b37da
Arunvel Sriram <arunvelsriram@gmail.com>
2014-12-29 22:20:37 +0530

    Generated JSON response and render it to template

--- scrapper/views.py
+++ scrapper/views.py
@@ -1,7 +1,8 @@
-from scrapper import app
+from . import app
 from flask import render_template, flash, request
 from forms import InputForm
 import datetime
+from .scrapper import Scrapper
 
 
 @app.route('/')
@@ -18,7 +19,10 @@ def fetch():
         else:
             register_no = form.register_no.data
             dob = datetime.datetime.strptime(str(form.dob.data), '%Y-%m-%d').strftime('%d-%m-%Y')
-
-            #return "Form posted {0} {1}".format(register_no, dob)
+            # return "Form posted {0} {1}".format(register_no, dob)
+            # fetch_details(register_no, dob)
+            s = Scrapper(register_no, dob)
+            json_data = s.get_json()
+            return render_template('test.html', form=form, data=json_data)
     elif request.method == 'GET':
         return render_template('test.html', form=form, title='Test Endpoint')
