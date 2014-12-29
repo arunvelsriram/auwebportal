42eb0e8f3aeea982c829b466b41e79429e2a2f5e
Arunvel Sriram <arunvelsriram@gmail.com>
2014-12-29 14:49:07 +0530

    Created a form and added validations

--- scrapper/forms.py
+++ scrapper/forms.py
@@ -1,1 +1,16 @@
-__author__ = 'arun'
+from flask.ext.wtf import Form
+from wtforms import StringField, DateField, validators
+
+
+class InputForm(Form):
+    register_no = StringField(
+        label='Register No',
+        validators=[validators.DataRequired(message='Register number required.'),
+                    validators.Length(min=8, max=20),
+        ]
+    )
+    dob = DateField(
+        label='Date of Birth',
+        format="%d-%m-%Y",
+        validators=[validators.DataRequired(message='Date of Birth required.')]
+    )
