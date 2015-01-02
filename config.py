b4d0a0eef78f4f715ad908340d5f13363886756d
Arunvel Sriram <arunvelsriram@gmail.com>
2015-01-02 19:34:40 +0530

    Added Openshift configurations

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
+import os
+
+DEBUG = True
+PROPAGATE_EXCEPTIONS = True
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'MyS3Cr3t'
+HOST_NAME = os.environ.get('OPENSHIFT_APP_DNS','localhost')
+APP_NAME = os.environ.get('OPENSHIFT_APP_NAME','auwebportal')
+IP = os.environ.get('OPENSHIFT_PYTHON_IP','127.0.0.1')
+PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT',8080))
