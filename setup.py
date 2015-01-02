2f02188083bd7787388ff4d544518d266d938960
Arunvel Sriram <arunvelsriram@gmail.com>
2015-01-02 19:37:04 +0530

    Configurations for deploying to openshift

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='auwebportal',
+      version='1.0',
+      description='A Python API using Flask microframework, to retrieve student details from Anna University web portal in JSON fromat.',
+      author='Arunvel Sriram',
+      author_email='arunvelsriram@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['Flask>=0.10.1', 'wtforms'],
+     )
